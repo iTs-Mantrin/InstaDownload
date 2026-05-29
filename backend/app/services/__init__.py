f8dbb330b6a84cb53453fb1494c58c5b6b190f06
Manish Kumar <manish.rajput62647457@gmail.com>
2026-05-30 03:30:28 +0530

    yt

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,5 +1,11 @@
-from .youtube_service import YouTubeService
+from .youtube_service import YouTubeService, get_celery_progress, clear_celery_progress
 from .instagram_service import InstagramService
 from .cleanup_service import CleanupService
 
-__all__ = ["YouTubeService", "InstagramService", "CleanupService"]
+__all__ = [
+    "YouTubeService",
+    "get_celery_progress",
+    "clear_celery_progress",
+    "InstagramService",
+    "CleanupService",
+]
